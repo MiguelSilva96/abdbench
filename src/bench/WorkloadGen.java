07001e553b9b8fe6fa21f8934c9000d1ce40c331
miguelsilva96 <a74601@alunos.uminho.pt>
2017-11-04 01:03:38 +0000

    Automatic testing for different number of threads

--- src/bench/WorkloadGen.java
+++ src/bench/WorkloadGen.java
@@ -3,14 +3,22 @@ package bench;
 
 import app.Store;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Random;
 
 public class WorkloadGen extends Thread {
-
+    private static final String URL = "jdbc:postgresql://localhost/invoices";
+    private static final String USERNAME = "miguel";
+    private static final String PASSWORD = "";
     private static long ultima = -1;
-
     private static long iaa = 0, tra = 0, c = 0;
     private static boolean start = false;
+
+
+    public boolean running = true;
 
     private static synchronized void regista(long antes, long depois) {
 
@@ -19,12 +27,10 @@ public class WorkloadGen extends Thread {
 
         long anterior = ultima;
         ultima = depois;
-
         if (anterior < 0 || !start)
             return;
 
         long ia = depois - anterior;
-
         iaa += ia;
         tra += tr;
         c++;
@@ -43,6 +49,7 @@ public class WorkloadGen extends Thread {
     }
 
 
+
     @Override
     public void run() {
         Store store = new Store();
@@ -50,7 +57,7 @@ public class WorkloadGen extends Thread {
         int productId, clientId, query;
 
 
-        while(true) {
+        while(running) {
             productId = rand.nextInt(Populate.PRODUCTS)|
                         rand.nextInt(Populate.PRODUCTS);
             clientId  = rand.nextInt(Populate.CLIENTS)|
@@ -72,26 +79,47 @@ public class WorkloadGen extends Thread {
         }
     }
 
+    public static void clearInvoices() {
+        Connection db;
+        String query;
+        PreparedStatement ps;
+        /*  Load driver class */
+        try {
+            Class.forName("org.postgresql.Driver");
+            db = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            query = "DELETE FROM invoice where invoice_id>=0;";
+            ps = db.prepareStatement(query);
+            ps.executeUpdate();
+            db.close();
+        } catch (ClassNotFoundException|SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String args[]) throws Exception {
-        int threads = 4;
+        int max_threads = 32;
+        int threads, current;
+        WorkloadGen[] workers = new WorkloadGen[max_threads];
 
-        //Some variable to keep track of times and throughput
-
-        while(threads-- > 0)
-            (new WorkloadGen()).start();
-
-        Thread.sleep(5000);
-
-        partida();
-
-        Thread.sleep(10000);
-
-        imprime();
+        for(threads = 1; threads <= max_threads; threads *= 2) {
+            current = 0;
+            while (current < threads) {
+                workers[current++] = new WorkloadGen();
+                workers[current-1].start();
+            }
+            Thread.sleep(5000);
+            partida();
+            Thread.sleep(10000);
+            System.out.printf("%d threads:\n", threads);
+            imprime();
+            for(int i = 0; i < threads; i++) {
+                workers[i].running = false;
+                workers[i].join();
+            }
+            clearInvoices();
+        }
 
         System.exit(0);
-        //Print benchmark info or export to csv file
-
     }
 
 }
